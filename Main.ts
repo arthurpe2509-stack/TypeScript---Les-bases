38aee12cce8bf8eb7fd6c7cd7845c4df07979359
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-05-07 15:06:27 +0200

    taille xs

--- Main.ts
+++ Main.ts
@@ -5,9 +5,10 @@ import { Order } from "./Order.js";
 import { Shoes } from "./product/Shoes.js";
 
 const customer = new Customer(1, "Alice Dupont", "alice@email.com");
-const product1 = new Clothing(1, "T-shirt", 0.2, 19.99, ClothingSize.Small);
+const product1 = new Clothing(1, "T-shirt", 0.2, 19.99, ClothingSize.ExtraSmall);
 const product2 = new Clothing(2, "Jeans", 0.5, 49.99, ClothingSize.Large);
 const product3 = new Shoes(3, "Sneakers", 0.8, 79.99, 42);
+
 
 const order = new Order(1, customer, [], new Date());
 order.addProduct(product1);
